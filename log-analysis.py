cbd49cff91156d319edf085a74ec4fddf3fcdf9c
yusefrich <yusef.rick@gmail.com>
2018-12-23 10:25:53 -0300

    correcao 1 - pep8 - query - readme

--- log-analysis.py
+++ log-analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import psycopg2
 
 DBNAME = "news"
@@ -6,62 +7,71 @@ DBNAME = "news"
 def article_analysis():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "select articles.title, count(log.path) as views " \
-            "from articles left join log " \
-            "on '/article/' || articles.slug like log.path " \
-            "group by articles.title " \
-            "order by views desc " \
-            "limit 3;"
+    query = """SELECT articles.title,
+                        count(log.path) AS views
+                FROM articles
+                LEFT JOIN log ON '/article/' || articles.slug LIKE log.path
+                GROUP BY articles.title
+                ORDER BY views DESC
+                LIMIT 3;"""
     c.execute(query)
     values = c.fetchall()
     db.close()
-    print("\nWhat are the most popular three articles of all time? \n")
-    for value in values:
-        print(
-            value[0], "Title",
-            " -- ", str(value[1]), "views")
+    print("\nWhat are the most popular three articles of all time?")
+    for title, views in values:
+        print("{} -- {} views".format(title, views))
 
 
 def author_analysis():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "select authors.name, articles.slug, count(log.path) as views from articles join log " \
-            "on'/article/' || articles.slug like log.path join authors " \
-            "on articles.author = authors.id " \
-            "group by authors.name, articles.slug " \
-            "order by views desc " \
-            "limit 3;"
+    query = """SELECT authors.name,
+                       count(log.path) AS views
+                FROM articles
+                JOIN log ON'/article/' || articles.slug LIKE log.path
+                JOIN authors ON articles.author = authors.id
+                GROUP BY authors.name
+                ORDER BY views DESC
+                LIMIT 3;"""
     c.execute(query)
     values = c.fetchall()
     db.close()
-    print("\nWho are the most popular three article authors of all time? \n")
-    for value in values:
-        print(
-            value[0], "Name",
-            " -- ", value[1], "Slug",
-            " -- ", str(value[2]), "views")
+    print("\nWho are the most popular three article authors of all time?")
+    for author, views in values:
+        print("{} -- {} views".format(author, views))
 
 
 def log_analysis():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    query = "select day, perc from " \
-            "(select day, round((sum(requests)/(select count(*) from log where " \
-            "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as " \
-            "perc from (select substring(cast(log.time as text), 0, 11) as day, " \
-            "count(*) as requests from log where status not like '%200%' group by day) " \
-            "as log_percentage group by day order by perc desc) as final_query " \
-            "where perc >= 1;"
+    query = """SELECT DAY,
+                       perc
+                FROM
+                  (SELECT DAY,
+                          round((sum(requests)/
+                                   (SELECT count(*)
+                                    FROM log
+                                    WHERE
+                                    TO_CHAR(log.time, 'Mon-DD,YYYY') = DAY) *
+                                    100), 2) AS perc
+                   FROM
+                     (SELECT TO_CHAR(log.time, 'Mon-DD,YYYY') AS DAY,
+                             count(*) AS requests
+                      FROM log
+                      WHERE status NOT LIKE '%200%'
+                      GROUP BY DAY) AS log_percentage
+                   GROUP BY DAY
+                   ORDER BY perc DESC) AS final_query
+                WHERE perc >= 1;"""
     c.execute(query)
     values = c.fetchall()
     db.close()
-    print("\nOn which days more than 1% of requests lead to errors? \n")
-    for value in values:
-        print(
-            value[0], "Day",
-            " -- ", str(value[1]), "Percent")
+    print("\nOn which days more than 1% of requests lead to errors?")
+    for day, percent in values:
+        print("{} -- {} % errors".format(day, percent))
 
 
-article_analysis()
-author_analysis()
-log_analysis()
+if __name__ == '__main__':
+    article_analysis()
+    author_analysis()
+    log_analysis()
